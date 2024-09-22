37519a63d037d5113139dbc3ec1ba6aeecbec420
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2024-09-22 18:48:37 +0800

    las variables en python introduccion

--- 1curso1.py
+++ 1curso1.py
@@ -5,3 +5,12 @@ print("Hola alumnos"); print("Adios mundo cruel")
 
 mi_nombre = "Mi nonbre es juan"
 print(mi_nombre)
+
+# La identacion es muy importante en python y no es 
+# mas que la forma en la que esta estruccturado nuestro codigo 
+
+a = 0
+
+for i in range(5):
+    a += 1
+    print(a)
