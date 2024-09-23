828bb23de572d7982362d33adeb83b2231db7e39
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2024-09-23 20:02:36 +0800

    Los diccionarios conceptos basicos

---
+++ 6curso.py
@@ -0,0 +1,43 @@
+# Los Diccionarios
+# Los son estructuras de datos que nos permiten almacenar datos de todo tipo, en el formato de clave: valor
+# Sintaxis
+miDiccionario = {
+    "Alemania": "Berlin",
+    "Francia": "Paris",
+    "EQG": "Malabo",
+    "Reino Unido": "Londres"
+}
+# Para acceder a los valores del diccionario tenemos que hacerlo con su clave o valor
+# Asignar valores a un diccionarios
+miDiccionario['Italia'] = 'Lisboa'
+print(miDiccionario)
+
+miDiccionario['Italia'] = 'Roma'
+print(miDiccionario)
+
+# eliminar un elemento del diccionario
+del miDiccionario['Reino Unido']
+print(miDiccionario)
+
+print("-------------------------------------Mezclas en diccionarios--------------------------------")
+
+miTupla = ('Reino Unido', 'Francia', 'Alemania')
+miDiccionario2 = {
+    miTupla[0]: 'Londres',
+    miTupla[1]: 'Paris',
+    miTupla[2]: 'Berlin'
+
+}
+
+print(miDiccionario2)
+
+miDiccionario3 = {
+    23: 'Jordan',
+    "Nombre": 'Michael',
+    "Equipo": "Chicago",
+    "Anillos": {'Temporadas': [1991, 19992, 1996, 1998]}
+}
+print(miDiccionario3.keys())
+print(miDiccionario3.values())
+print(len(miDiccionario3))
+print(miDiccionario3)
