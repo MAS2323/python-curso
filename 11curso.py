e2aeb9865db577bb0229b51f7a45210d3a57ca0a
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2024-09-23 21:40:18 +0800

    Los condicionales ejercicio

---
+++ 11curso.py
@@ -0,0 +1,7 @@
+# Bucle for
+# Existen ducles determinados e indeterminados
+# Bucle for determinado: sintaxis for variable in elemento a recorer:
+# cuerpo del bucle
+
+for i in [1, 2, 3, 4]:
+    print("Hola")
