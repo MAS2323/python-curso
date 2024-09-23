004f86dee3bcde3b7da34fc8eb6c5bd533f568f1
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2024-09-23 14:06:25 +0800

    conceptos basicos de python

--- 2curso.py
+++ 2curso.py
@@ -1,8 +1,8 @@
-print("Hola Mundo!!")
-print("El weta " * 4)
+# Tipos, operadores y variables
+# En python vamos a manejar tres tipos de datos: numericos, textos y booleanos
 
-print()
+# Los operadores que podemos usar en python son los siguientes
+# Aritmeticos, comparacion, logicos, asignacion y especiales
 
-mas-onewe = "Hola"
-a = 4
-b = 3
+# Variable: es un espacio en la memoria del ordenador donde se almacena un valor, que podra cambiar durante
+# la ejecucion del programa
