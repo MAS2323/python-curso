5756a78b9bb1baef52d4e8b1d68410a6da8d1784
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2024-09-23 22:11:32 +0800

    Bucle for conceptos basicos

--- 11curso.py
+++ 11curso.py
@@ -3,5 +3,28 @@
 # Bucle for determinado: sintaxis for variable in elemento a recorer:
 # cuerpo del bucle
 
-for i in [1, 2, 3, 4]:
-    print("Hola")
+# for i in [1, 2, 3, 4]:
+#     print("Hola")
+# for i in ['primavera', 'verano', 'otono', 'invierno']:
+#     print(i)
+
+contador = 0
+miEmail = input("Introduce tu direccion de email: ")
+
+for i in miEmail:
+
+    # # El agumento end determina como queremos que se imprima nuestro bucle
+    # print('hola', end="")
+
+    if (i == '@' or i == "."):
+        contador = contador + 1
+if contador == 2:
+    print("Email es correcto")
+else:
+    print("El email es incorrecto")
+
+# -----------------------------------------------------------------------------------------
+# El tipo range() es una funcion que crea un array que empieza desde el 0 has n
+
+for i in range(5):
+    print('Hola')
