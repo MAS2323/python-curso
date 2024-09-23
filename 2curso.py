7564bf5a4f06af750fc8c4d398ac251b19b6d622
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2024-09-23 14:14:05 +0800

    Habalndo sobre lo que establece una variable

--- 2curso.py
+++ 2curso.py
@@ -6,3 +6,12 @@
 
 # Variable: es un espacio en la memoria del ordenador donde se almacena un valor, que podra cambiar durante
 # la ejecucion del programa
+
+# En python el tipo de variable no lo establece el contenedor si no el contenido
+
+nombre = "Mas onewe"
+
+a = 3
+b = 6
+
+print(nombre)
