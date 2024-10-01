fbaf4cd230018f53e9d756d894aa417a1c3794db
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2024-10-01 16:35:45 +0800

    creacion de paquetes distribuibles

---
+++ setup.py
@@ -0,0 +1,12 @@
+#
+from setuptools import setup
+
+setup(
+    name="Paquetecalculos",
+    version="1.0",
+    description="Paquetes de redondeo y potencia",
+    author="Mas Onewe",
+    author_email="masoneweernesto@gmail.com",
+    url="www.programacion.com",
+    packages=["calculos", "calculos.redondeo_potencia"]
+)
